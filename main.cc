1e1fe442c18ec9f837bd71f7d246e8fd99402eb9
Razvan <razvanboghiu@outlook.com>
2025-02-08 17:08:15 +0000

    Add parallel to parallel communication in 2 phases

--- main.cc
+++ main.cc
@@ -12,6 +12,7 @@ struct device_info {
     union ibv_gid gid;
     uint32_t send_qp_num, write_qp_num;
     struct ibv_mr write_mr;
+    struct ibv_mr send_mr;
 };
 
 struct rdma_context_per_peer {
@@ -29,7 +30,6 @@ struct rdma_context_per_peer {
 };
 
 struct rdma_context {
-    bool server = false;
     int num_devices = 0;
     uint32_t gidIndex = 0;
     string ip_str, dev_str;
@@ -55,7 +55,6 @@ void parse_program_options(int argc, char *argv[], rdma_context &ctx) {
             ("dev", boost::program_options::value<string>(), "rdma device to use")
             ("src_ip", boost::program_options::value<string>(), "source ip")
             ("dst_ip", boost::program_options::value<vector<string> >()->multitoken(), "destination ips")
-            ("server", "run as server")
             ("message", boost::program_options::value<string>(), "Message to send");
 
     boost::program_options::variables_map vm;
@@ -85,9 +84,6 @@ void parse_program_options(int argc, char *argv[], rdma_context &ctx) {
         }
     else
         cerr << "the --dst_ip argument is required" << endl;
-
-    if (vm.count("server"))
-        ctx.server = true;
 
     if (vm.count("message")) {
         ctx.message = vm["message"].as<string>();
@@ -256,28 +252,29 @@ void find_gid(rdma_context &ctx) {
 }
 
 void register_memory(rdma_context &ctx) {
-    auto flags = IBV_ACCESS_LOCAL_WRITE |
-                 IBV_ACCESS_REMOTE_WRITE |
-                 IBV_ACCESS_REMOTE_READ;
+    auto flags = IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE | IBV_ACCESS_REMOTE_READ;
 
     for (int i = 0; i < ctx.peers.size(); i++) {
-        // register the "data_send" and "data_write" buffers for RDMA operations, using ibv_reg_mr;
-        // store the resulting mrs in send_mr and write_mr
-        ctx.peers[i].send_mr = ibv_reg_mr(ctx.pd, ctx.peers[i].data_send, sizeof(ctx.peers[i].data_send), flags);
+        ctx.peers[i].send_mr = ibv_reg_mr(ctx.pd, ctx.peers[i].data_send,
+                                          sizeof(ctx.peers[i].data_send), flags);
         if (!ctx.peers[i].send_mr) {
-            cerr << "ibv_reg_mr failed: " << strerror(errno) << endl;
+            cerr << "ibv_reg_mr failed for send_mr: " << strerror(errno) << endl;
             ibv_destroy_qp(ctx.peers[i].write_qp);
             exit(1);
         }
 
-        ctx.peers[i].write_mr = ibv_reg_mr(ctx.pd, ctx.peers[i].data_write, sizeof(ctx.peers[i].data_write), flags);
+        ctx.peers[i].write_mr = ibv_reg_mr(ctx.pd, ctx.peers[i].data_write,
+                                           sizeof(ctx.peers[i].data_write), flags);
         if (!ctx.peers[i].write_mr) {
-            cerr << "ibv_reg_mr failed: " << strerror(errno) << endl;
+            cerr << "ibv_reg_mr failed for write_mr: " << strerror(errno) << endl;
             ibv_dereg_mr(ctx.peers[i].send_mr);
             exit(1);
         }
 
+        // Copy both into ctx.peers[i].local
         memcpy(&ctx.peers[i].local.write_mr, ctx.peers[i].write_mr, sizeof(ctx.peers[i].local.write_mr));
+        memcpy(&ctx.peers[i].local.send_mr, ctx.peers[i].send_mr, sizeof(ctx.peers[i].local.send_mr));
+
         ctx.peers[i].local.send_qp_num = ctx.peers[i].send_qp->qp_num;
         ctx.peers[i].local.write_qp_num = ctx.peers[i].write_qp->qp_num;
     }
@@ -373,222 +370,255 @@ void modify_qps_rts(rdma_context &ctx) {
     }
 }
 
-int send_data(const struct device_info &data, string ip) {
-    int sockfd;
-    struct sockaddr_in servaddr;
-
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1)
-        return 1;
-
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = inet_addr(ip.c_str());
-    servaddr.sin_port = htons(8080);
-
-    if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) != 0)
-        return 1;
-
-    write(sockfd, &data, sizeof(data));
-    close(sockfd);
-
-    return 0;
+static in_addr_t parse_ip(const std::string &ip_str) {
+    in_addr addr;
+    inet_aton(ip_str.c_str(), &addr);
+    return addr.s_addr;
 }
 
-int receive_data(struct device_info &data) {
-    int sockfd, connfd, len;
-    struct sockaddr_in servaddr;
+int all_to_all_exchange_data(rdma_context &ctx) {
+    cout << "[DEBUG] Entering all-to-all exchange" << endl;
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1)
-        return 1;
-
-    memset(&servaddr, 0, sizeof(servaddr));
-
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(8080);
-
-    if ((bind(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr))) != 0)
-        return 1;
-
-    if ((listen(sockfd, 5)) != 0)
-        return 1;
-
-    connfd = accept(sockfd, NULL, NULL);
-    if (connfd < 0)
-        return 1;
-
-    read(connfd, &data, sizeof(data));
-
-    close(sockfd);
-
-    return 0;
-}
-
-int receive_data_for_all_peers(vector<device_info> &peers) {
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        return 1;
-    }
-
+    // Common listening socket for 'accept' side
+    int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenfd < 0) return 1;
     int reuse = 1;
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-        return 1;
-    }
-
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
     sockaddr_in servaddr;
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(8080);
+    if (bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) return 1;
+    if (listen(listenfd, ctx.peers.size() * 2) < 0) return 1;
 
-    if (bind(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
-        return 1;
-    }
-    if (listen(sockfd, peers.size()) < 0) {
-        return 1;
-    }
+    in_addr_t local_ip = parse_ip(ctx.ip_str);
 
-    for (int i = 0; i < peers.size(); i++) {
-        int connfd = accept(sockfd, nullptr, nullptr);
-        if (connfd < 0) {
-            return 1;
-        }
-        read(connfd, &peers[i], sizeof(peers[i]));
-        close(connfd);
-    }
-    close(sockfd);
-    return 0;
-}
-
-int exchange_data_info(rdma_context &ctx) {
-    if (ctx.server) {
-        // Accept connections from all peers
-        vector<device_info> remotes(ctx.peers.size());
-        if (receive_data_for_all_peers(remotes) != 0) {
-            return 1;
-        }
-
-        // Copy what was received into each peer
-        for (int i = 0; i < ctx.peers.size(); i++) {
-            ctx.peers[i].remote = remotes[i];
-            if (send_data(ctx.peers[i].local, ctx.peers[i].remote_ip_str) != 0) {
-                return 1;
-            }
-        }
-    } else {
-        for (auto &p: ctx.peers) {
-            if (send_data(p.local, p.remote_ip_str) != 0) {
-                return 1;
-            }
-            if (receive_data(p.remote) != 0) {
-                return 1;
-            }
-        }
-    }
-    return 0;
-}
-
-
-void server_flow(rdma_context &ctx) {
     for (int i = 0; i < ctx.peers.size(); i++) {
+        in_addr_t remote_ip = parse_ip(ctx.peers[i].remote_ip_str);
+        bool iAmSmaller = (ntohl(local_ip) < ntohl(remote_ip));
+
+        if (iAmSmaller) {
+            bool connected = false;
+            for (int attempts = 0; attempts < 10; attempts++) {
+                int cfd = socket(AF_INET, SOCK_STREAM, 0);
+                if (cfd < 0) {
+                    cout << "[DEBUG] Socket creation failed for peer " << i << endl;
+                    continue;
+                }
+                sockaddr_in peeraddr;
+                memset(&peeraddr, 0, sizeof(peeraddr));
+                peeraddr.sin_family = AF_INET;
+                peeraddr.sin_addr.s_addr = remote_ip;
+                peeraddr.sin_port = htons(8080);
+
+                if (connect(cfd, (struct sockaddr *) &peeraddr, sizeof(peeraddr)) == 0) {
+                    cout << "[DEBUG] Connected to " << ctx.peers[i].remote_ip_str << endl;
+                    write(cfd, &ctx.peers[i].local, sizeof(ctx.peers[i].local));
+                    read(cfd, &ctx.peers[i].remote, sizeof(ctx.peers[i].remote));
+                    close(cfd);
+                    connected = true;
+                    break;
+                } else {
+                    cout << "[DEBUG] Connect failed with " << ctx.peers[i].remote_ip_str
+                            << " (attempt " << attempts + 1 << "), retrying..." << endl;
+                    close(cfd);
+                    sleep(1);
+                }
+            }
+            if (!connected) {
+                cout << "[DEBUG] Failed to connect after retries for peer " << i << endl;
+                return 1;
+            }
+        } else {
+            cout << "[DEBUG] Waiting for inbound connection from " << ctx.peers[i].remote_ip_str << endl;
+            int cfd = accept(listenfd, nullptr, nullptr);
+            if (cfd < 0) {
+                cout << "[DEBUG] Accept failed for peer " << i << endl;
+                return 1;
+            }
+            read(cfd, &ctx.peers[i].remote, sizeof(ctx.peers[i].remote));
+            write(cfd, &ctx.peers[i].local, sizeof(ctx.peers[i].local));
+            close(cfd);
+        }
+    }
+
+    close(listenfd);
+    cout << "[DEBUG] All-to-all exchange completed" << endl;
+    return 0;
+}
+
+static int barrier_handshake(rdma_context &ctx, int phase) {
+    int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    int reuse = 1;
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+    sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(8081);
+    bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+    listen(listenfd, ctx.peers.size() * 2);
+
+    in_addr addrLocal;
+    inet_aton(ctx.ip_str.c_str(), &addrLocal);
+    for (int i = 0; i < (int) ctx.peers.size(); i++) {
+        in_addr addrRemote;
+        inet_aton(ctx.peers[i].remote_ip_str.c_str(), &addrRemote);
+        bool smaller = (ntohl(addrLocal.s_addr) < ntohl(addrRemote.s_addr));
+        if (smaller) {
+            bool ok = false;
+            for (int attempt = 0; attempt < 10; attempt++) {
+                int cfd = socket(AF_INET, SOCK_STREAM, 0);
+                sockaddr_in peer;
+                memset(&peer, 0, sizeof(peer));
+                peer.sin_family = AF_INET;
+                peer.sin_addr.s_addr = addrRemote.s_addr;
+                peer.sin_port = htons(8081);
+                if (connect(cfd, (sockaddr *) &peer, sizeof(peer)) == 0) {
+                    write(cfd, &phase, sizeof(phase));
+                    int p2 = 0;
+                    read(cfd, &p2, sizeof(p2));
+                    close(cfd);
+                    ok = true;
+                    break;
+                }
+                close(cfd);
+                sleep(1);
+            }
+            if (!ok) {
+                close(listenfd);
+                return 1;
+            }
+        } else {
+            int cfd = accept(listenfd, nullptr, nullptr);
+            int p1 = 0;
+            read(cfd, &p1, sizeof(p1));
+            write(cfd, &phase, sizeof(phase));
+            close(cfd);
+        }
+    }
+    close(listenfd);
+    return 0;
+}
+
+void all_to_all_flow(rdma_context &ctx) {
+    for (int i = 0; i < (int) ctx.peers.size(); i++) {
         memset(ctx.peers[i].data_write, 0, sizeof(ctx.peers[i].data_write));
         memset(ctx.peers[i].data_send, 0, sizeof(ctx.peers[i].data_send));
         memcpy(ctx.peers[i].data_write, ctx.message.c_str(), sizeof(ctx.peers[i].data_write) - 1);
-
-        memset(&ctx.sg_write, 0, sizeof(ctx.sg_write));
-        ctx.sg_write.addr = (uintptr_t) ctx.peers[i].write_mr->addr;
-        ctx.sg_write.length = sizeof(ctx.peers[i].data_write);
-        ctx.sg_write.lkey = ctx.peers[i].write_mr->lkey;
-
-        memset(&ctx.wr_write, 0, sizeof(ctx.wr_write));
-        ctx.wr_write.wr_id = 0;
-        ctx.wr_write.sg_list = &ctx.sg_write;
-        ctx.wr_write.num_sge = 1;
-        ctx.wr_write.opcode = IBV_WR_RDMA_WRITE_WITH_IMM;
-        ctx.wr_write.send_flags = IBV_SEND_SIGNALED;
-        ctx.wr_write.imm_data = htonl(0x1234);
-        ctx.wr_write.wr.rdma.remote_addr = (uintptr_t) ctx.peers[i].remote.write_mr.addr;
-        ctx.wr_write.wr.rdma.rkey = ctx.peers[i].remote.write_mr.rkey;
-
-        int ret = ibv_post_send(ctx.peers[i].write_qp, &ctx.wr_write, &ctx.bad_wr_write);
-        if (ret != 0) {
-            exit(1);
-        }
-
         memset(&ctx.sg_recv, 0, sizeof(ctx.sg_recv));
         ctx.sg_recv.addr = (uintptr_t) ctx.peers[i].send_mr->addr;
         ctx.sg_recv.length = sizeof(ctx.peers[i].data_send);
         ctx.sg_recv.lkey = ctx.peers[i].send_mr->lkey;
-
         memset(&ctx.wr_recv, 0, sizeof(ctx.wr_recv));
-        ctx.wr_recv.wr_id = 0;
         ctx.wr_recv.sg_list = &ctx.sg_recv;
         ctx.wr_recv.num_sge = 1;
-        ret = ibv_post_recv(ctx.peers[i].send_qp, &ctx.wr_recv, &ctx.bad_wr_recv);
-        if (ret != 0) {
-            exit(1);
+        ibv_post_recv(ctx.peers[i].write_qp, &ctx.wr_recv, &ctx.bad_wr_recv);
+        memset(&ctx.sg_write, 0, sizeof(ctx.sg_write));
+        ctx.sg_write.addr = (uintptr_t) ctx.peers[i].write_mr->addr;
+        ctx.sg_write.length = sizeof(ctx.peers[i].data_write);
+        ctx.sg_write.lkey = ctx.peers[i].write_mr->lkey;
+        memset(&ctx.wr_write, 0, sizeof(ctx.wr_write));
+        ctx.wr_write.sg_list = &ctx.sg_write;
+        ctx.wr_write.num_sge = 1;
+        ctx.wr_write.opcode = IBV_WR_RDMA_WRITE_WITH_IMM;
+        ctx.wr_write.send_flags = IBV_SEND_SIGNALED;
+        ctx.wr_write.imm_data = htonl(0xABCD);
+        ctx.wr_write.wr.rdma.remote_addr = (uintptr_t) ctx.peers[i].remote.send_mr.addr;
+        ctx.wr_write.wr.rdma.rkey = ctx.peers[i].remote.send_mr.rkey;
+        ibv_post_send(ctx.peers[i].write_qp, &ctx.wr_write, &ctx.bad_wr_write);
+        while (!ibv_poll_cq(ctx.peers[i].write_cq, 1, &ctx.wc)) {
         }
-
-        do {
-            ret = ibv_poll_cq(ctx.peers[i].send_cq, 1, &ctx.wc);
-        } while (ret == 0);
-
-        if (ctx.wc.status != IBV_WC_SUCCESS) {
-            exit(1);
-        }
-
-        cout << ctx.peers[i].data_send << endl;
+        cout << "[DEBUG] PHASE1 received from peer " << i << ": " << ctx.peers[i].data_send << endl;
     }
-}
+    barrier_handshake(ctx, 1);
 
-void client_flow(rdma_context &ctx) {
-    for (int i = 0; i < ctx.peers.size(); i++) {
+    for (int i = 0; i < (int) ctx.peers.size(); i++) {
         memset(ctx.peers[i].data_write, 0, sizeof(ctx.peers[i].data_write));
         memset(ctx.peers[i].data_send, 0, sizeof(ctx.peers[i].data_send));
-        memcpy(ctx.peers[i].data_send, ctx.message.c_str(), sizeof(ctx.peers[i].data_write) - 1);
-
+        string msg2 = ctx.message + " [Phase2]";
+        memcpy(ctx.peers[i].data_write, msg2.c_str(), sizeof(ctx.peers[i].data_write) - 1);
         memset(&ctx.sg_recv, 0, sizeof(ctx.sg_recv));
-        ctx.sg_recv.addr = (uintptr_t) ctx.peers[i].write_mr->addr;
-        ctx.sg_recv.length = sizeof(ctx.peers[i].data_write);
-        ctx.sg_recv.lkey = ctx.peers[i].write_mr->lkey;
-
+        ctx.sg_recv.addr = (uintptr_t) ctx.peers[i].send_mr->addr;
+        ctx.sg_recv.length = sizeof(ctx.peers[i].data_send);
+        ctx.sg_recv.lkey = ctx.peers[i].send_mr->lkey;
         memset(&ctx.wr_recv, 0, sizeof(ctx.wr_recv));
-        ctx.wr_recv.wr_id = 0;
         ctx.wr_recv.sg_list = &ctx.sg_recv;
         ctx.wr_recv.num_sge = 1;
-
-        int ret = ibv_post_recv(ctx.peers[i].write_qp, &ctx.wr_recv, &ctx.bad_wr_recv);
-        if (ret != 0) {
-            exit(1);
+        ibv_post_recv(ctx.peers[i].write_qp, &ctx.wr_recv, &ctx.bad_wr_recv);
+        memset(&ctx.sg_write, 0, sizeof(ctx.sg_write));
+        ctx.sg_write.addr = (uintptr_t) ctx.peers[i].write_mr->addr;
+        ctx.sg_write.length = sizeof(ctx.peers[i].data_write);
+        ctx.sg_write.lkey = ctx.peers[i].write_mr->lkey;
+        memset(&ctx.wr_write, 0, sizeof(ctx.wr_write));
+        ctx.wr_write.sg_list = &ctx.sg_write;
+        ctx.wr_write.num_sge = 1;
+        ctx.wr_write.opcode = IBV_WR_RDMA_WRITE_WITH_IMM;
+        ctx.wr_write.send_flags = IBV_SEND_SIGNALED;
+        ctx.wr_write.imm_data = htonl(0xEEFF);
+        ctx.wr_write.wr.rdma.remote_addr = (uintptr_t) ctx.peers[i].remote.send_mr.addr;
+        ctx.wr_write.wr.rdma.rkey = ctx.peers[i].remote.send_mr.rkey;
+        ibv_post_send(ctx.peers[i].write_qp, &ctx.wr_write, &ctx.bad_wr_write);
+        while (!ibv_poll_cq(ctx.peers[i].write_cq, 1, &ctx.wc)) {
         }
+        cout << "[DEBUG] PHASE2 received from peer " << i << ": " << ctx.peers[i].data_send << endl;
+    }
+    barrier_handshake(ctx, 2);
+    cout << "[DEBUG] All-to-all flow (two-phase + barrier) completed" << endl;
+}
 
-        do {
-            ret = ibv_poll_cq(ctx.peers[i].write_cq, 1, &ctx.wc);
-        } while (ret == 0);
-        if (ctx.wc.status != IBV_WC_SUCCESS) {
-            exit(1);
+
+void cleanup_rdma(rdma_context &ctx) {
+    cout << "[DEBUG] Starting cleanup." << endl;
+
+    // Destroy QPs, deregister MRs, and destroy CQs for each peer
+    for (int i = 0; i < ctx.peers.size(); i++) {
+        if (ctx.peers[i].send_qp) {
+            ibv_destroy_qp(ctx.peers[i].send_qp);
+            ctx.peers[i].send_qp = nullptr;
         }
-
-        cout << ctx.peers[i].data_write << endl;
-
-        memset(&ctx.sg_send, 0, sizeof(ctx.sg_send));
-        ctx.sg_send.addr = (uintptr_t) ctx.peers[i].send_mr->addr;
-        ctx.sg_send.length = sizeof(ctx.peers[i].data_send);
-        ctx.sg_send.lkey = ctx.peers[i].send_mr->lkey;
-
-        memset(&ctx.wr_send, 0, sizeof(ctx.wr_send));
-        ctx.wr_send.wr_id = 0;
-        ctx.wr_send.sg_list = &ctx.sg_send;
-        ctx.wr_send.num_sge = 1;
-        ctx.wr_send.opcode = IBV_WR_SEND;
-        ctx.wr_send.send_flags = IBV_SEND_SIGNALED;
-
-        ret = ibv_post_send(ctx.peers[i].send_qp, &ctx.wr_send, &ctx.bad_wr_send);
-        if (ret != 0) {
-            exit(1);
+        if (ctx.peers[i].write_qp) {
+            ibv_destroy_qp(ctx.peers[i].write_qp);
+            ctx.peers[i].write_qp = nullptr;
+        }
+        if (ctx.peers[i].send_mr) {
+            ibv_dereg_mr(ctx.peers[i].send_mr);
+            ctx.peers[i].send_mr = nullptr;
+        }
+        if (ctx.peers[i].write_mr) {
+            ibv_dereg_mr(ctx.peers[i].write_mr);
+            ctx.peers[i].write_mr = nullptr;
+        }
+        if (ctx.peers[i].send_cq) {
+            ibv_destroy_cq(ctx.peers[i].send_cq);
+            ctx.peers[i].send_cq = nullptr;
+        }
+        if (ctx.peers[i].write_cq) {
+            ibv_destroy_cq(ctx.peers[i].write_cq);
+            ctx.peers[i].write_cq = nullptr;
         }
     }
+
+    // Deallocate the PD
+    if (ctx.pd) {
+        ibv_dealloc_pd(ctx.pd);
+        ctx.pd = nullptr;
+    }
+
+    // Close the device
+    if (ctx.context) {
+        ibv_close_device(ctx.context);
+        ctx.context = nullptr;
+    }
+
+    // Free the device list
+    if (ctx.dev_list) {
+        ibv_free_device_list(ctx.dev_list);
+        ctx.dev_list = nullptr;
+    }
+
+    cout << "[DEBUG] Cleanup complete." << endl;
 }
 
 
@@ -602,15 +632,16 @@ int main(int argc, char *argv[]) {
     modify_qps_init(ctx);
     find_gid(ctx);
     register_memory(ctx);
-    if (exchange_data_info(ctx) != 0) exit(1);
+    all_to_all_exchange_data(ctx);
 
     modify_qps_rtr(ctx);
     modify_qps_rts(ctx);
 
-    if (ctx.server)
-        server_flow(ctx);
-    else
-        client_flow(ctx);
+    all_to_all_flow(ctx);
+
+    sleep(1);
+    cleanup_rdma(ctx);
+    sleep(1);
 
     return 0;
 }
